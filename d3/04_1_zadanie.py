3613f222acaa7d5d9ede30378f98e4af427050cc
MartaLP <marta.lys-pisowacka@acxiom.com>
2018-10-13 13:45:16 +0200

    zadania w jedym pliku

--- d3/04_1_zadanie.py
+++
@@ -1,8 +0,0 @@
-# 1. czy liczba jest podzielna przez 3 lub 5 lub 7
-
-liczba = float(input("Podaj liczbe: \n"))
-
-if liczba % 3 == 0 or liczba % 5 == 0 or liczba % 7 == 0:
-    print("Liczba {} jest podzielna przez 3 lub 5 lub 7".format(liczba))
-else:
-    print("Liczba {} nie jest podzielna przez 3, 5 lub 7".format(liczba))
